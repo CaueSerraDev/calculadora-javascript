5e02b7d92329f48a61809fd146308080ce5e98e6
Cauê Serra <eusoucaueserra@gmail.com>
2026-03-05 17:13:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//Funções
+
+function somar(a,b) { return a+b;}
+function subtrair(a,b) { return a-b;}
+function multiplicar(a,b) { return a*b;}
+
+function dividir(a,b){
+if(b==0)
+    alert("Erro, divisão por zero, escolha outro número!")
+return a/b;
+}
+
+function exponencial(a,b) {
+    return Math.pow(a,b); // Math.pow(a, b) calcula 'a' elevado à potência 'b'
+}
+
